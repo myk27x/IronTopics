d94d8990fb544e66702ba38a33fcbef936c9cce1
Michael Reed <myk27x@yahoo.com>
2015-11-17 12:39:31 -0500

    removing turbolinks and test console log

--- app/assets/javascripts/topic_listing.js
+++ app/assets/javascripts/topic_listing.js
@@ -3,7 +3,6 @@ $(document).ready(function()
   $("#topics").on("click", ".topic_listing :not(a)", function()
   {
     var id = $(this).parent(".topic_listing").data('topic-id');
-    console.log("this ID is" + id)
 
     $(".modal-content").html("");
 
